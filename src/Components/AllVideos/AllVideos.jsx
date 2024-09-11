458737cc65a7cb1b9399748951e615933e330b57
mabdulrahman <chabdulrahmangill58@gmail.com>
2024-09-11 13:08:55 +0500

    navbar updated

--- src/Components/AllVideos/AllVideos.jsx
+++ src/Components/AllVideos/AllVideos.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const AllVideos = () => {
   const [videos, setVideos] = useState([]);
   const navigate = useNavigate();
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/upload';
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL 
 
   // Fetch video data
   useEffect(() => {
