c972a71f2397d24d1b0c3809eed8cc9061ee3b7f
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-10-28 11:32:06 +0500

    env exp

--- src/API.js
+++ src/API.js
@@ -1,6 +1,7 @@
 // src/api.js
 import axios from "axios";
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+import {REACT_APP_API_BASE_URL} from './ENV'
+const API_BASE_URL = REACT_APP_API_BASE_URL;
 
 export const getUserId = () => {
   const str = document.cookie;
